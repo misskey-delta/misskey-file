803da192b1e67544dd5fc18780d836bef0aa9232
syuilo <syuilotan@yahoo.co.jp>
2015-11-05 03:21:23 +0900

    Implement validations

--- src/api/rename.ts
+++ src/api/rename.ts
@@ -5,18 +5,23 @@ import config from '../config';
 module.exports = (req: express.Request, res: express.Response) => {
 	const passkey: string = req.body['passkey'];
 	if (passkey === config.passkey) {
-		const oldPath: string = `${config.storagePath}/${req.body['old-path']}`;
-		const newPath: string =
-			`${config.storagePath}/${req.body['old-path'].substring(0, req.body['old-path'].lastIndexOf('/'))}/${req.body['new-name']}`;
-
-		fs.rename(oldPath, newPath, (err: NodeJS.ErrnoException) => {
-			if (err !== null) {
-				console.log(err);
-				res.sendStatus(500);
-			} else {
-				res.send(newPath);
-			}
-		});
+		const newName: string = req.body['new-name'];
+		if (newName.indexOf('..') > -1 || newName.indexOf('/') > -1 || newName.indexOf('\\') > -1) {
+			res.sendStatus(400);
+		} else {
+			const oldPath: string = `${config.storagePath}/${req.body['old-path']}`;
+			const newPath: string =
+				`${config.storagePath}/${req.body['old-path'].substring(0, req.body['old-path'].lastIndexOf('/'))}/${newName}`;
+	
+			fs.rename(oldPath, newPath, (err: NodeJS.ErrnoException) => {
+				if (err !== null) {
+					console.log(err);
+					res.sendStatus(500);
+				} else {
+					res.send(newPath);
+				}
+			});
+		}
 	} else {
 		res.sendStatus(400);
 	}
