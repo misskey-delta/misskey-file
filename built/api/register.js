7525a58d83a65b8a834eb07c22a957c288c314d7
syuilo <syuilotan@yahoo.co.jp>
2015-12-07 05:42:34 +0900

    commit

--- built/api/register.js
+++ built/api/register.js
@@ -8,59 +8,60 @@ var gm = require('gm');
 var config_1 = require('../config');
 module.exports = function (req, res) {
     var passkey = req.body['passkey'];
-    if (passkey === config_1.default.passkey) {
-        if (Object.keys(req.files).length === 1) {
-            (function () {
-                var fileId = req.body['file-id'];
-                var file = req.files['file'];
-                var tmppath = file.path;
-                var fileName = file.originalname;
-                var fileBuffer = fs.readFileSync(tmppath);
-                fs.unlink(tmppath);
-                var publicPath = fileId + '/' + fileName;
-                var privatePath = config_1.default.storagePath + '/' + publicPath;
-                mkdirp(path.dirname(privatePath), function (mkdirErr) {
-                    if (mkdirErr !== null) {
-                        console.error(mkdirErr);
-                        return res.sendStatus(500);
-                    }
-                    fs.writeFile(privatePath, fileBuffer, function (writeFileErr) {
-                        if (writeFileErr !== null) {
-                            console.error(writeFileErr);
-                            return res.sendStatus(500);
-                        }
-                        var detectedFileType = fileType(fileBuffer);
-                        if (detectedFileType === null) {
+    var file = req.file;
+    if (passkey !== config_1.default.passkey) {
+        return res.sendStatus(400);
+    }
+    if (file === undefined || file === null) {
+        return res.sendStatus(400);
+    }
+    var fileId = req.body['file-id'];
+    var tmppath = file.path;
+    var fileName = file.originalname;
+    var fileBuffer = fs.readFileSync(tmppath);
+    fs.unlink(tmppath);
+    var publicPath = fileId + '/' + fileName;
+    var privatePath = config_1.default.storagePath + '/' + publicPath;
+    mkdirp(path.dirname(privatePath), function (mkdirErr) {
+        if (mkdirErr !== null) {
+            console.error(mkdirErr);
+            return res.sendStatus(500);
+        }
+        fs.writeFile(privatePath, fileBuffer, function (writeFileErr) {
+            if (writeFileErr !== null) {
+                console.error(writeFileErr);
+                return res.sendStatus(500);
+            }
+            var detectedFileType = fileType(fileBuffer);
+            if (detectedFileType === null) {
+                return res.send(publicPath);
+            }
+            switch (detectedFileType.mime) {
+                case 'image/jpeg':
+                case 'image/png':
+                case 'image/gif':
+                case 'image/webp':
+                case 'image/tiff':
+                case 'image/bmp':
+                    mkdirp(config_1.default.storagePath + '/' + fileId + '/minified', function (mkdirErr) {
+                        if (mkdirErr !== undefined && mkdirErr !== null) {
+                            console.error(mkdirErr);
                             return res.send(publicPath);
                         }
-                        switch (detectedFileType.mime) {
-                            case 'image/jpeg':
-                            case 'image/png':
-                            case 'image/gif':
-                            case 'image/webp':
-                            case 'image/tiff':
-                            case 'image/bmp':
-                                mkdirp(config_1.default.storagePath + '/' + fileId + '/minified', function (mkdirErr) {
-                                    gm(privatePath).resize(150, 150).compress('jpeg').quality('80').toBuffer('jpeg', function (genThumbnailErr, thumbnail) {
-                                        if (genThumbnailErr !== undefined && genThumbnailErr !== null) {
-                                            console.error(genThumbnailErr);
-                                        }
-                                        fs.writeFile(config_1.default.storagePath + '/' + fileId + '/minified/' + fileName, thumbnail, function (writeFileErr) {
-                                            res.send(publicPath);
-                                        });
-                                    });
-                                });
-                                break;
-                            default:
+                        gm(privatePath).resize(150, 150).compress('jpeg').quality('80').toBuffer('jpeg', function (genThumbnailErr, thumbnail) {
+                            if (genThumbnailErr !== undefined && genThumbnailErr !== null) {
+                                console.error(genThumbnailErr);
                                 return res.send(publicPath);
-                        }
+                            }
+                            fs.writeFile(config_1.default.storagePath + '/' + fileId + '/minified/' + fileName, thumbnail, function (writeFileErr) {
+                                res.send(publicPath);
+                            });
+                        });
                     });
-                });
-            })();
-        } else {
-            res.sendStatus(400);
-        }
-    } else {
-        res.sendStatus(400);
-    }
+                    break;
+                default:
+                    return res.send(publicPath);
+            }
+        });
+    });
 };
--- src/api/register.ts
+++ src/api/register.ts
@@ -9,64 +9,69 @@ import config from '../config';
 
 module.exports = (req: express.Request, res: express.Response) => {
 	const passkey: string = req.body['passkey'];
-	if (passkey === config.passkey) {
-		if (Object.keys(req.files).length === 1) {
-			const fileId: string = req.body['file-id'];
-			const file: Express.Multer.File = req.files['file'];
-			const tmppath: string = file.path;
-			const fileName: string = file.originalname;
-			const fileBuffer: Buffer = fs.readFileSync(tmppath);
-			fs.unlink(tmppath);
+	const file: Express.Multer.File = (<any>req).file;
 
-			const publicPath: string = `${fileId}/${fileName}`;
-			const privatePath: string = `${config.storagePath}/${publicPath}`;
-			mkdirp(path.dirname(privatePath), (mkdirErr: any) => {
-				if (mkdirErr !== null) {
-					console.error(mkdirErr);
-					return res.sendStatus(500);
-				}
-				fs.writeFile(privatePath, fileBuffer, (writeFileErr: NodeJS.ErrnoException) => {
-					if (writeFileErr !== null) {
-						console.error(writeFileErr);
-						return res.sendStatus(500);
-					}
-
-					const detectedFileType = fileType(fileBuffer);
-					if (detectedFileType === null) {
-						return res.send(publicPath);
-					}
-					switch (detectedFileType.mime) {
-						case 'image/jpeg':
-						case 'image/png':
-						case 'image/gif':
-						case 'image/webp':
-						case 'image/tiff':
-						case 'image/bmp':
-							mkdirp(`${config.storagePath}/${fileId}/minified`, (mkdirErr: any) => {
-								// サムネイル生成
-								gm(privatePath)
-								.resize(150, 150)
-								.compress('jpeg')
-								.quality('80')
-								.toBuffer('jpeg', (genThumbnailErr: Error, thumbnail: Buffer) => {
-									if (genThumbnailErr !== undefined && genThumbnailErr !== null) {
-										console.error(genThumbnailErr);
-									}
-									fs.writeFile(`${config.storagePath}/${fileId}/minified/${fileName}`, thumbnail,  (writeFileErr: NodeJS.ErrnoException) => {
-										res.send(publicPath);
-									});
-								});
-							});
-							break;
-						default:
-							return res.send(publicPath);
-					}
-				});
-			});
-		} else {
-			res.sendStatus(400);
-		}
-	} else {
-		res.sendStatus(400);
+	if (passkey !== config.passkey) {
+		return res.sendStatus(400);
 	}
+	if (file === undefined || file === null) {
+		return res.sendStatus(400);
+	}
+
+	const fileId: string = req.body['file-id'];
+	const tmppath: string = file.path;
+	const fileName: string = file.originalname;
+	const fileBuffer: Buffer = fs.readFileSync(tmppath);
+	fs.unlink(tmppath);
+
+	const publicPath: string = `${fileId}/${fileName}`;
+	const privatePath: string = `${config.storagePath}/${publicPath}`;
+	mkdirp(path.dirname(privatePath), (mkdirErr: any) => {
+		if (mkdirErr !== null) {
+			console.error(mkdirErr);
+			return res.sendStatus(500);
+		}
+		fs.writeFile(privatePath, fileBuffer, (writeFileErr: NodeJS.ErrnoException) => {
+			if (writeFileErr !== null) {
+				console.error(writeFileErr);
+				return res.sendStatus(500);
+			}
+
+			const detectedFileType = fileType(fileBuffer);
+			if (detectedFileType === null) {
+				return res.send(publicPath);
+			}
+			switch (detectedFileType.mime) {
+				case 'image/jpeg':
+				case 'image/png':
+				case 'image/gif':
+				case 'image/webp':
+				case 'image/tiff':
+				case 'image/bmp':
+					mkdirp(`${config.storagePath}/${fileId}/minified`, (mkdirErr: any) => {
+						if (mkdirErr !== undefined && mkdirErr !== null) {
+							console.error(mkdirErr);
+							return res.send(publicPath);
+						}
+						// サムネイル生成
+						gm(privatePath)
+						.resize(150, 150)
+						.compress('jpeg')
+						.quality('80')
+						.toBuffer('jpeg', (genThumbnailErr: Error, thumbnail: Buffer) => {
+							if (genThumbnailErr !== undefined && genThumbnailErr !== null) {
+								console.error(genThumbnailErr);
+								return res.send(publicPath);
+							}
+							fs.writeFile(`${config.storagePath}/${fileId}/minified/${fileName}`, thumbnail,  (writeFileErr: NodeJS.ErrnoException) => {
+								res.send(publicPath);
+							});
+						});
+					});
+					break;
+				default:
+					return res.send(publicPath);
+			}
+		});
+	});
 };
